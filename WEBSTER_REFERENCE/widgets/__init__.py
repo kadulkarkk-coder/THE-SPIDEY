73f9882adfbbc9c51854d7ac1142e9996b9e3526
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-04 23:27:47 +0530

    feat(widgets): expose widget data and clock components

--- WEBSTER_REFERENCE/widgets/__init__.py
+++ WEBSTER_REFERENCE/widgets/__init__.py
@@ -1,1 +1,7 @@
 """WEBSTER Desktop Widgets reference package."""
+
+from .clock_widget import ClockWidget
+from .widget_data import WidgetData, WidgetDataError
+from .widget_store import WidgetDataStore
+
+__all__ = ["ClockWidget", "WidgetData", "WidgetDataError", "WidgetDataStore"]
