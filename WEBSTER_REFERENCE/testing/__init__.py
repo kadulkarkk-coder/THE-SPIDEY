c741ed6a9ef8da49cf059555f675c82a417cd7b3
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-06 15:38:18 +0530

    feat(learning): expose local learning foundation

--- WEBSTER_REFERENCE/testing/__init__.py
+++ WEBSTER_REFERENCE/testing/__init__.py
@@ -1,12 +1,26 @@
-"""WEBSTER Testing reference package."""
+"""WEBSTER Testing and local learning reference package."""
 
+from .failure_patterns import FailurePatternDetector
+from .improvement_candidates import ImprovementCandidate, ImprovementCandidateBuilder
 from .integration_suite import IntegrationReport, IntegrationSuite
+from .learning_loop import LearningCycle, LocalLearningLoop
+from .learning_records import LearningRecord, LearningStore
+from .performance_analyzer import PerformanceAnalyzer, PerformanceSnapshot
 from .test_result import TestResult, TestSummary
 from .test_runner import TestRunner
 
 __all__ = [
+    "FailurePatternDetector",
+    "ImprovementCandidate",
+    "ImprovementCandidateBuilder",
     "IntegrationReport",
     "IntegrationSuite",
+    "LearningCycle",
+    "LocalLearningLoop",
+    "LearningRecord",
+    "LearningStore",
+    "PerformanceAnalyzer",
+    "PerformanceSnapshot",
     "TestResult",
     "TestSummary",
     "TestRunner",
