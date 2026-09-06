3b2f3d8a029189cdcae55e0d6ef9a2dd5b86bca8
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-06 12:58:22 +0530

    Expose Sprint 63 testing contracts

--- WEBSTER_REFERENCE/testing/__init__.py
+++ WEBSTER_REFERENCE/testing/__init__.py
@@ -1,1 +1,13 @@
 """WEBSTER Testing reference package."""
+
+from .integration_suite import IntegrationReport, IntegrationSuite
+from .test_result import TestResult, TestSummary
+from .test_runner import TestRunner
+
+__all__ = [
+    "IntegrationReport",
+    "IntegrationSuite",
+    "TestResult",
+    "TestSummary",
+    "TestRunner",
+]
