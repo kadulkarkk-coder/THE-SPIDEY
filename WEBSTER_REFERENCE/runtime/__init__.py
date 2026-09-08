22ad2d639a6a1e54e581b18496f63851d9827abc
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-08 22:04:02 +0530

    WEBSTER A1: add functional runtime package

---
+++ WEBSTER_REFERENCE/runtime/__init__.py
@@ -0,0 +1,6 @@
+"""Functional runtime services for the WEBSTER Mark D rebuild."""
+
+from .runtime_manager import RuntimeManager
+from .runtime_state import RuntimeState
+
+__all__ = ["RuntimeManager", "RuntimeState"]
