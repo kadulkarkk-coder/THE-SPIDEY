fef848455622266d07738fcb0e02406cf356a70f
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-17 11:23:20 +0530

    WEBSTER A2: export runtime request bridge

--- WEBSTER_REFERENCE/runtime/__init__.py
+++ WEBSTER_REFERENCE/runtime/__init__.py
@@ -1,6 +1,7 @@
 """Functional runtime services for the WEBSTER Mark D rebuild."""
 
+from .request_bridge import RuntimeRequestBridge, RuntimeRequestEvent
 from .runtime_manager import RuntimeManager
 from .runtime_state import RuntimeState
 
-__all__ = ["RuntimeManager", "RuntimeState"]
+__all__ = ["RuntimeManager", "RuntimeState", "RuntimeRequestBridge", "RuntimeRequestEvent"]
