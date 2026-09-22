035ef4a0144d4f8e2293e0b492e43bddb47aa126
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-22 15:24:50 +0530

    A18: export execution verification

--- WEBSTER_REFERENCE/intelligence/__init__.py
+++ WEBSTER_REFERENCE/intelligence/__init__.py
@@ -8,10 +8,11 @@ from .local_knowledge import LocalKnowledge
 from .local_provider import LocalProvider
 from .response_composer import ComposedResponse, ResponseComposer
 from .robust_planner import RobustPlanner, VerifiedPlan, VerifiedStep
+from .execution_verifier import ExecutionVerifier, VerificationResult
 
 __all__ = [
     "Decision", "DecisionEngine", "OfflineProvider", "ProviderResponse",
     "LocalProvider", "LocalKnowledge", "calculate",
     "ConversationState", "ConversationTurn", "ContextBuilder", "BuiltContext",
-    "ResponseComposer", "ComposedResponse", "RobustPlanner", "VerifiedPlan", "VerifiedStep",
+    "ResponseComposer", "ComposedResponse", "RobustPlanner", "VerifiedPlan", "VerifiedStep", "ExecutionVerifier", "VerificationResult",
 ]
