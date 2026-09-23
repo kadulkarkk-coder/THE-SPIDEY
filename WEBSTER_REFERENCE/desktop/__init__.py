d2f4503578987ad7d3e23ea01699222cb215e74e
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-23 16:27:57 +0530

    Restart B1-B5: export functional desktop capabilities

--- WEBSTER_REFERENCE/desktop/__init__.py
+++ WEBSTER_REFERENCE/desktop/__init__.py
@@ -1,1 +1,15 @@
-"""WEBSTER Desktop and System Control reference package."""
+"""WEBSTER real desktop/system capabilities."""
+
+from .desktop_runtime import DesktopAction, DesktopRuntime
+from .system_interface import SystemInterface, SystemSnapshot
+from .process_discovery import ProcessDiscovery, ProcessMatch
+from .window_control import WindowController, WindowInfo
+from .keyboard_driver import KeyboardDriver
+
+__all__ = [
+    "DesktopAction", "DesktopRuntime",
+    "SystemInterface", "SystemSnapshot",
+    "ProcessDiscovery", "ProcessMatch",
+    "WindowController", "WindowInfo",
+    "KeyboardDriver",
+]
