00b55ef07087db523393d53ae8896829679c7c9f
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-09-23 16:43:47 +0530

    Restart B20: export unified desktop stack

--- WEBSTER_REFERENCE/desktop/__init__.py
+++ WEBSTER_REFERENCE/desktop/__init__.py
@@ -1,15 +1,16 @@
-"""WEBSTER real desktop/system capabilities."""
-
-from .desktop_runtime import DesktopAction, DesktopRuntime
-from .system_interface import SystemInterface, SystemSnapshot
-from .process_discovery import ProcessDiscovery, ProcessMatch
-from .window_control import WindowController, WindowInfo
-from .keyboard_driver import KeyboardDriver
-
-__all__ = [
-    "DesktopAction", "DesktopRuntime",
-    "SystemInterface", "SystemSnapshot",
-    "ProcessDiscovery", "ProcessMatch",
-    "WindowController", "WindowInfo",
-    "KeyboardDriver",
-]
+"""WEBSTER functional desktop and system control."""
+from .desktop_controller import DesktopController
+from .desktop_runtime import DesktopRuntime, DesktopAction
+from .mouse_driver import MouseDriver
+from .clipboard_control import ClipboardControl
+from .file_operations import FileOperations
+from .window_actions import WindowActions
+from .display_info import DisplayInfo
+from .system_settings import SystemSettings
+from .safe_shell import SafeShell
+from .browser_control import BrowserControl
+from .screenshot_control import ScreenshotControl
+from .hotkey_manager import HotkeyManager
+from .desktop_audit import DesktopAuditTrail
+from .desktop_permissions import DesktopPermissions
+__all__=["DesktopController","DesktopRuntime","DesktopAction","MouseDriver","ClipboardControl","FileOperations","WindowActions","DisplayInfo","SystemSettings","SafeShell","BrowserControl","ScreenshotControl","HotkeyManager","DesktopAuditTrail","DesktopPermissions"]
